742592a659f0543c576d05048ce984d1b44ef152
Miguel Morales <miguel.morales@24imedia.com>
2021-01-12 17:12:06 +0100

    feat: Show percentage in the votes bar.

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,17 +4,19 @@ import { MdCancel } from 'react-icons/md'
 
 class Option extends Component {
 
-  percentage = (votes, total) => (votes * 100)/total
+  percentage = (votes, total) => Math.floor((votes * 100)/total)
 
   render() {
     const { option, totalVotes, voted } = this.props
+    const percentage = this.percentage(option.votes.length, totalVotes)
 
     return (
         <div className='answer'>
           <div className={'option ' + (voted ? 'voted' : '')}>
             <span>{`Would you rather ${option.text}?`}</span>
-            <div className='percentage'>
-              <div className='fill' style={{width: this.percentage(option.votes.length, totalVotes) + '%'}}></div>
+            <div className='percentage-bar'>
+              <div className='fill' style={{width: percentage + '%'}}></div>
+              <span className='percentage'>{percentage + '%'}</span>
             </div>
             <span>{`${option.votes.length} votes of totalVotes`}</span>
           </div>
